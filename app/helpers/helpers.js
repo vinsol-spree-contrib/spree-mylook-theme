90392e1207acaa7f0f8bf76e96f3624a23aba38a
vijayta <vijayta.panchal@gmail.com>
2016-12-28 16:33:12 +0530

    theme design

--- app/helpers/helpers.js
+++ app/helpers/helpers.js
@@ -1,3 +1,3 @@
 Handlebars.registerHelper('productImageUrl', function(image_ids, images) {
-  // return images.find(function(elem){return elem.id == image_ids[0]}).large_url;
+  return images.find(function(elem){return elem.id == image_ids[0]}).large_url;
 });
