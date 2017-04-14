9b08c0c69307c43e620aa5f9d654e53404b40c8a
vijayta <vijayta@vinsol.com>
2017-04-14 17:38:45 +0530

    dynamic navigation bar for categories

--- app/helpers/helpers.js
+++ app/helpers/helpers.js
@@ -3,30 +3,86 @@ Handlebars.registerHelper('productUrl', function(id) {
 });
 
 Handlebars.registerHelper('productImageUrl', function(image_ids, images) {
-  return images.find(function(elem) { return elem.id == image_ids[0]}).large_url;
+  return images.find(function(element) { return element.id == image_ids[0] }).large_url;
 });
 
 Handlebars.registerHelper('productCategoryUrl', function(id) {
-  return 'category_products/' + id
-});
-
-Handlebars.registerHelper('getChildCategories', function(allCategories, parentId) {
-  var categories = []
-  $.each(allCategories, function(index, category){
-    alert (category.parentId);
-    if(category.parentId === parentId){
-      categories.push(category)
-    }
-  });
-  return categories;
+  return 'category_products/' + id;
 });
 
 Handlebars.registerHelper('isLoggedIn', function(options) {
-  var cookie_auth_key = "; auth_token=";
+  var cookie_auth_key = "auth_token=";
 
   if(document.cookie.indexOf(cookie_auth_key) > -1) {
     return options.fn(this);
   } else {
     return options.inverse(this);
   }
+});
+
+Handlebars.registerHelper('isOrderComplete', function(completedAt, options) {
+  if(completedAt != null) {
+    return options.fn(this);
+  }
+});
+
+Handlebars.registerHelper('isParent', function(id, options) {
+  if(id == null) {
+    return options.fn(this);
+  }
+});
+
+Handlebars.registerHelper('isChild', function(categories, taxonomy_id, options) {
+  var child = categories.map(function(elem, i){
+    if(elem.taxonomy_id == taxonomy_id){
+      return elem;
+    }
+  });
+  childs = child.filter(function(element){ return element != undefined });
+  return options.fn(childs);
+});
+
+Handlebars.registerHelper('firstItem', function(index, className) {
+  return index == 0 ? className : '';
+});
+
+Handlebars.registerHelper('formatDate', function(date) {
+  return new Date(date).toDateString();
+});
+
+Handlebars.registerHelper('showAddress', function(address_id, address, options) {
+  var _address = address.find(function(element){return element.id == address_id});
+  return options.fn(_address);
+});
+
+Handlebars.registerHelper('showShipping', function(shipment_ids, shipments, shipping_rates, options) {
+  var _shipment = shipments.find(function(element){return element.id == shipment_ids[0]});
+  var _shipmentNumber = _shipment.selected_shipping_rate_id;
+  var _rate = shipping_rates.find(function(element){return element.id == _shipmentNumber});
+  return options.fn(_rate);
+});
+
+Handlebars.registerHelper('showPayment', function(payment_ids, payments, payment_methods, options) {
+  var _payment = payments.find(function(element){return element.id == payment_ids[0]});
+  var _paymentNumber = _payment.payment_method_id;
+  var _method = payment_methods.find(function(element){return element.id == _paymentNumber});
+  return options.fn(_method);
+});
+
+Handlebars.registerHelper('showVariants', function(line_item_id, line_items, variants, images, options) {
+  var _lineItem = line_items.find(function(element) { return element.id == line_item_id });
+  var _variantId = _lineItem.variant_id;
+  var _variant = variants.find(function(element){return element.id == _variantId});
+  var _imageId = _variant.image_ids[0];
+  var _image = images.find(function(element){return element.id == _imageId});
+  var itemsHash = { 'line_item': _lineItem, 'variant': _variant, 'image': _image };
+  return options.fn(itemsHash);
+});
+
+Handlebars.registerHelper('showProductVariants', function(variant_id, variants, images, options) {
+  var _variant = variants.find(function(element) { return element.id == variant_id});
+  var _imageId = _variant.image_ids[0];
+  var _image = images.find(function(element){return element.id == _imageId});
+  var itemsHash = { 'variant': _variant, 'image': _image };
+  return options.fn(itemsHash);
 });
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,76 +1,156 @@
-import { SpreeApi } from './../../bower_components/spree-frontend-integration/lib/index'
-function renderIndexPage(responseText){
-  document.querySelector('#wrapper').innerHTML = MyApp.html.index({products: responseText['products'], images: responseText['images']});
-};
-function renderProductShowPage(responseText) {
-  document.querySelector('#wrapper').innerHTML = MyApp.html.show({product: responseText.product, productProperties: responseText.product_properties, images: responseText['images']});
+import { SpreeApi } from './../../bower_components/spree-frontend-integration/lib/index';
+import { getCookie, setCookie } from './cookie'
+
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1));
+  var sURLVariables = sPageURL.split('&');
+  var sParameterName;
+  var i;
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
 };
 
-function renderMenPage(responseText){
-  document.querySelector('#wrapper').innerHTML = MyApp.html.men({products: responseText['products'], images: responseText['images']});
+function getActivePage(page) {
+  var activePage = Number(decodeURIComponent(window.location.search.substring(1)).split('=')[1]);
+  if(isNaN(activePage)) {
+    $('.to-be-hidden').removeClass('hide');
+    $('.pagination li').first().addClass('active');
+  }
+  $('.pagination').find('li').each(function() {
+    if($(this).index() + 1 == activePage) {
+      $('.pagination li').removeClass('active');
+      $(this).addClass('active');
+    }
+  });
+  if(activePage > page) {
+    $('.products-found').hide();
+    $('.to-be-hidden').addClass('hide');
+    $('.products-not-found').removeClass('hide');
+  }
 };
 
-function renderWomenPage(responseText){
-  document.querySelector('#wrapper').innerHTML = MyApp.html.women({products: responseText['products'], images: responseText['images']});
-};
+function renderHomePage(taxonData, productsData) {
+  document.querySelector('#wrapper').innerHTML = MyApp.html.index({
+    products: productsData['products'],
+    images: productsData['images'],
+    categories: taxonData['taxons']
+  });
+}
 
-function renderKidsPage(responseText){
-  document.querySelector('#wrapper').innerHTML = MyApp.html.women({products: responseText['products'], images: responseText['images']});
-};
-
-function renderSalesPage(responseText){
-  document.querySelector('#wrapper').innerHTML = MyApp.html.sales({products: responseText['products'], images: responseText['images']});
-};
-
-function renderFreshArrivalPage(responseText){
-  document.querySelector('#wrapper').innerHTML = MyApp.html.fresh_arrival({products: responseText['products'], images: responseText['images']});
+function renderIndexPage(responseText) {
+  new SpreeApi.taxonomyList().sendRequest({cb: function(taxonData){
+    renderHomePage(taxonData, responseText)
+  }});
 };
 
 function renderCategoryPage(responseText) {
-  document.querySelector('#wrapper').innerHTML = MyApp.html.categories({products: responseText['products'], images: responseText['images'], categories: responseText['taxonomies'], categoryName: responseText.taxons});
+  document.querySelector('#wrapper').innerHTML = MyApp.html.categories({
+    categories: responseText['taxons']
+  });
 };
 
 function renderCategoryProductsPage(responseText) {
-  document.querySelector('#wrapper').innerHTML = MyApp.html.category_products({products: responseText['products'], category_name: responseText.category_name, images: responseText['images']});
+  document.querySelector('#wrapper').innerHTML = MyApp.html.category_products({
+    products: responseText['products'],
+    images: responseText['images'],
+    meta: responseText['meta']
+  });
+  var activePage = Number(window.location.pathname.split('/')[2]);
+  var totalPages = responseText.meta.total_pages;
+  if(totalPages <= 1) {
+    $('.pagination-row .pagination').addClass('hide');
+  }
+  for(var index = 1; index <= totalPages; index++) {
+    $('.pagination-row .pagination').append('<li><a href="/category_products/' + activePage + '?page=' + index + '">' + index + '</a></li>');
+  }
+  getActivePage(totalPages);
+};
+
+function renderProductShowPage(responseText) {
+  document.querySelector('#wrapper').innerHTML = MyApp.html.show({
+    product: responseText['product'],
+    productProperties: responseText['product_properties'],
+    images: responseText['images'],
+    variants: responseText['variants']
+  });
+};
+
+function renderAllProductsPage(responseText) {
+  document.querySelector('#wrapper').innerHTML = MyApp.html.products({
+    product: responseText['product'],
+    images: responseText['images'],
+  });
+};
+
+function renderMyOrders(responseText) {
+  document.querySelector('#wrapper').innerHTML = MyApp.html.my_orders({
+    addresses: responseText['addresses'],
+    orders: responseText['orders'],
+    line_items: responseText['line_items'],
+    payment_methods: responseText['payment_methods'],
+    payments: responseText['payments'],
+    shipments: responseText['shipments'],
+    shipping_rates: responseText['shipping_rates'],
+    variants: responseText['variants'],
+    images: responseText['images']
+  });
+};
+
+function renderCart() {
+  document.querySelector('#wrapper').innerHTML = MyApp.html.cart();
+};
+
+function renderCheckout() {
+  document.querySelector('#wrapper').innerHTML = MyApp.html.checkout();
+};
+
+function createOrder(response) {
+  setCookie('orderId', response['order']['number']);
+};
+
+function getOrderDetails(responseText) {
+  window.order = responseText;
 };
 
 function renderTemplate() {
+  if(getCookie('orderId').length == 0) {
+    (new SpreeApi.createOrder()).sendRequest({ cb: createOrder })
+  }
+  (new SpreeApi.currentOrder()).sendRequest({cb: getOrderDetails, path: '/api/ams/orders/' + getCookie('orderId')})
+
   var path = window.location.pathname.split('/')[1],
       id = window.location.pathname.split('/')[2],
       category_id = window.location.pathname.split('/')[2];
-  if(path == ''){
-    // (new SpreeApi.productsList()).sendRequest({cb: renderIndexPage})
-    (new SpreeApi.productsList()).sendRequest({params:{ per_page: 4}})
-  }
 
-  if(path == 'men'){
-    (new SpreeApi.productsList()).sendRequest({params:{ per_page: 4, page: 1}, cb: renderMenPage})
-  }
-
-  if(path == 'women'){
-    (new SpreeApi.productsList()).sendRequest({cb: renderWomenPage})
-  }
-
-  if(path == 'kids'){
-    (new SpreeApi.productsList()).sendRequest({cb: renderKidsPage})
-  }
-
-  if(path == 'sales'){
-    (new SpreeApi.productsList()).sendRequest({cb: renderSalesPage})
-  }
-
-  if(path == 'fresh_arrival'){
-    (new SpreeApi.productsList()).sendRequest({cb: renderFreshArrivalPage})
-  }
   switch(path) {
-    case 'categories' : (new SpreeApi.taxonomyList()).sendRequest({cb: renderCategoryPage});
-    break;
-    case 'product' : (new SpreeApi.productShow()).sendRequest({cb: renderProductShowPage, params: { id: id }});
-    break;
-    case 'category_products' : (new SpreeApi.productsList()).sendRequest({cb: renderCategoryProductsPage, params: { q: { taxons_taxonomy_id_eq: category_id, category_name: name }}});
-    break;
-    default : (new SpreeApi.productsList()).sendRequest({cb: renderIndexPage});
+    case 'categories':
+      new SpreeApi.taxonomyList().sendRequest({cb: renderCategoryPage});
+      break;
+    case 'product':
+      new SpreeApi.productShow().sendRequest({cb: renderProductShowPage, params: { id: id }});
+      break;
+    case 'category_products':
+      new SpreeApi.productsList().sendRequest({cb: renderCategoryProductsPage, params: { q: { taxons_id_eq: category_id }, per_page: 6, page: getUrlParameter('page') }});
+      break;
+    case 'cart':
+      new SpreeApi.productsList().sendRequest({cb: renderCart});
+      break;
+    case 'checkout':
+      new SpreeApi.productsList().sendRequest({cb: renderCheckout});
+      break;
+    case 'my_orders':
+      new SpreeApi.myOrders().sendRequest({cb: renderMyOrders, path: '/api/ams/orders', method: 'GET'})
+      break;
+    case 'products':
+      new SpreeApi.productsList().sendRequest({cb: renderAllProductsPage});
+      break;
+    default:
+      new SpreeApi.productsList().sendRequest({cb: renderIndexPage, params: {per_page: 8}});
   }
-}
+};
 
 renderTemplate();
